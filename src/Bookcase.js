131736d135e4f9278ddeccc98207c978a77adab8
QinghangPeng <voyagejk.peng@gmail.com>
2021-05-12 21:55:01 +0800

    to extract components

--- src/Bookcase.js
+++ src/Bookcase.js
@@ -1,4 +1,6 @@
 import { Component, Fragment } from "react";
+import './style.css'
+import BookcaseItem from './BookcaseItem'
 
 class Bookcase extends Component {
     //构造器
@@ -10,18 +12,54 @@ class Bookcase extends Component {
         }
     }
 
+    //生命周期
+    // componentWillMount() {
+    //     console.log('componentWillMount  ---- 组件将要挂载到页面的时刻')
+    // }
+
+    // componentDidMount() {
+    //     console.log('componentDidMount  ---- 组件挂载完成的时刻')
+    // }
+
+    // shouldComponentUpdate() {
+    //     console.log('1-shouldComponentUpdate')
+    //     return true
+    // }
+
+    // componentWillUpdate() {
+    //     console.log('2-componentWillUpdate')
+    // }
+
+    // componentDidUpdate() {
+    //     console.log('4-componentDidUpdate')
+    // }
+
     render() {
+        console.log('3-render  ---- 组件挂载中')
         return (
             //flex
             <Fragment>
+                {/* 这是一个div的注释 */}
                 <div>
-                    <input value = {this.state.inputValue} onChange = {this.inputChange.bind(this)}/>
+                    <label htmlFor = "jp">增加书籍：</label>
+                    <input id = "jp" className = "input" value = {this.state.inputValue} onChange = {this.inputChange.bind(this)}/>
                     <button onClick = {this.addListItem.bind(this)}>增加书籍</button>
                 </div>
                 <ul>
                     {
                         this.state.list.map((item,index) => {
-                            return <li key = {item + index}>{item}</li>
+                            return (
+                                /*
+                                <li key = {item + index}
+                                    onClick = {this.deleteItem.bind(this,index)}
+                                    dangerouslySetInnerHTML = {{__html:item}}
+                                >
+                                </li>
+                                */
+                                <div>
+                                    <BookcaseItem />
+                                </div>
+                            )
                         })
                     }
                 </ul>
@@ -41,6 +79,15 @@ class Bookcase extends Component {
         this.setState({
             list: [...this.state.list,this.state.inputValue],
             inputValue: ''
+        })
+    }
+
+    deleteItem(index) {
+        console.log(index)
+        let list = this.state.list
+        list.splice(index,1)
+        this.setState({
+            list: list
         })
     }
 
---
+++ src/BookcaseItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class BookcaseItem extends Component {
+    render() { 
+        return ( 
+            <li>重构改善既有代码的设计</li>
+         );
+    }
+}
+
+export default BookcaseItem
+ 
