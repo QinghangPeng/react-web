1b32d19c9d170ac9859a39f24d81132d2538c9ae
QinghangPeng <voyagejk.peng@gmail.com>
2021-05-14 00:21:29 +0800

    use react-transition-group to implement animation

--- src/Boos.js
+++ src/Boos.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { CSSTransition } from 'react-transition-group'
 
 class Boss extends Component {
     constructor(props) {
@@ -12,7 +13,15 @@ class Boss extends Component {
     render() { 
         return ( 
             <div>
-                <div className = {this.state.isShow? 'show':'hide'}>最新活动 - 五折</div>
+                <CSSTransition 
+                    in = {this.state.isShow}
+                    timeout = {2000}
+                    classNames = "boss-text"
+                    unmountOnExit
+                >
+                    <div>最新活动 - 五折</div>
+                </CSSTransition>
+                {/* <div className = {this.state.isShow? 'show':'hide'}>最新活动 - 五折</div> */}
                 <div><button onClick = {this.toToggole}>参加活动</button></div>
             </div>
          );
